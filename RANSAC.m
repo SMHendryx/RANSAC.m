998fe19350d0c9ebfe63230d50a85aa9d2769258
SMHendryx <seanmhendryx@gmail.com>
2018-02-11 12:12:07 -0700

    adding copyright and license

--- RANSAC.m
+++ RANSAC.m
@@ -1,3 +1,19 @@
+%Copyright 2018 Sean M. Hendryx
+%
+%Licensed under the Apache License, Version 2.0 (the "License");
+%you may not use this file except in compliance with the License.
+%You may obtain a copy of the License at
+%
+%    http://www.apache.org/licenses/LICENSE-2.0
+%
+%Unless required by applicable law or agreed to in writing, software
+%distributed under the License is distributed on an "AS IS" BASIS,
+%WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%See the License for the specific language governing permissions and
+%limitations under the License.
+
+% RANSAC implemenation in Matlab for returning hypothesized model and inliers.
+
 function m = homogenousLS(U)
     % Given the design matrix U, returns the parameters of the homogenous
     % least squares solution IN STANDARD FORM: ax + by = d
